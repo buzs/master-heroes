bb0ef33ed94c733a9a9232f01dd6837ff1c00fcb
Gabriel Henrique <contato@gabrielhenrique.net>
2020-03-13 17:29:14 -0300

    change react class to fc

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Typography, Button } from 'antd';
 import { CheckCircleOutlined } from '@ant-design/icons';
 
 import { Card } from './styles';
+import Link from 'next/link';
 
-const { Title, Text, Paragraph } = Typography;
+const { Title, Text } = Typography;
 
-export default class Welcome extends Component {
-	render() {
-		return (
-			<Card>
-				<Title level={2}>
-					Welcome to Master Hero
-				</Title>
-				<Text className="sub">
-					this game will test if you
-					really know heroes
+export default function Welcome() {
+	return (
+		<Card>
+			<Title level={2}>
+				Welcome to Master Hero
+			</Title>
+			<Text className="sub">
+				this game will test if you really
+				know heroes
+			</Text>
+
+			<Text className="desc">
+				the game consists of you selecting
+				the right hero name from the image
+			</Text>
+
+			<div className="info-list">
+				<Text>
+					<CheckCircleOutlined /> there
+					will be 10 questions
 				</Text>
-
-				<Text className="desc">
-					the game consists of you
-					selecting the right hero name
-					from the image
+				<Text>
+					<CheckCircleOutlined /> each
+					hit counts 20 points
 				</Text>
-
-				<div className="info-list">
-					<Text>
-						<CheckCircleOutlined />{' '}
-						there will be 10 questions
-					</Text>
-					<Text>
-						<CheckCircleOutlined />{' '}
-						each hit counts 20 points
-					</Text>
-				</div>
-
+			</div>
+			<Link href="/play">
 				<Button type="primary">
 					Start Challenge
 				</Button>
-			</Card>
-		);
-	}
+			</Link>
+		</Card>
+	);
 }
