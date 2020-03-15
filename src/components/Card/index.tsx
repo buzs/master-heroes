718eaed2b3f5132a0b8b08711d425b3ab3189250
Gabriel Henrique <contato@gabrielhenrique.net>
2020-03-14 23:14:43 -0300

    add card styles in components

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  background: #fff;
+  max-width: 380px;
+  padding: 30px 20px;
+  border-radius: 2px;
+  display: flex;
+
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+
+  img {
+    max-width: 160px;
+    margin-right: 20px;
+  }
+
+  .nameOptions {
+    min-width: 160px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+`;
