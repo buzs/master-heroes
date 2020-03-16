cfc358fa8597ff6077a0d0b44c63fc1c65161256
Gabriel Henrique <contato@gabrielhenrique.net>
2020-03-16 02:51:56 -0300

    fix redux and add redux-persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,38 @@
-import withReduxStore from '../lib/with-redux-store';
+import App, { AppProps } from 'next/app';
 import React from 'react';
+import withReduxStore from '../lib/with-redux-store';
 import { Provider } from 'react-redux';
-import { AppProps } from 'next/app';
+import { persistStore, Persistor } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import GlobalStyle from '../src/styles/global';
 
 export interface AppPropsWithRedux extends AppProps {
-  reduxStore: any;
+  reduxStore?: any;
 }
 
-function MyApp({ Component, pageProps, reduxStore }: AppPropsWithRedux) {
-  return (
-    <Provider store={reduxStore}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </Provider>
-  );
+class MyApp extends App {
+  persistor: Persistor;
+  constructor(props: AppPropsWithRedux) {
+    super(props);
+    this.persistor = persistStore(props.reduxStore);
+  }
+
+  render() {
+    //@ts-ignore
+    const { Component, pageProps, reduxStore } = this.props;
+    return (
+      <Provider store={reduxStore}>
+        <PersistGate
+          loading={<Component {...pageProps} />}
+          persistor={this.persistor}
+        >
+          <Component {...pageProps} />
+          <GlobalStyle />
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
 
 export default withReduxStore(MyApp);
